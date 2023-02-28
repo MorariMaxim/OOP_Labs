0b49deaa07ed00caacbe99407bdd6f173267abbb
MorariMaxim <119452648+MorariMaxim@users.noreply.github.com>
2023-02-28 19:31:21 +0200

    Update GlobalFunctions.cpp

--- lab2/2/GlobalFunctions.cpp
+++ lab2/2/GlobalFunctions.cpp
@@ -1,29 +1,29 @@
 #include "GlobalFunctions.h"
 #include <iostream>
 int compareEnglish(Student* s1,  Student* s2) {
-	int eng1 = s1->getEnglish();
-	int eng2 = s2->getEnglish();
+	float eng1 = s1->getEnglish();
+	float eng2 = s2->getEnglish();
 	if (eng1 == eng2) return 0;
 	if (eng1 > eng2) return 1;
 	if (eng1 < eng2) return -1;
 }
 int compareMathematics( Student* s1,  Student* s2) {
-	int math1 = s1->getMathsematics();
-	int math2 = s2->getMathsematics();
+	float math1 = s1->getMathsematics();
+	float math2 = s2->getMathsematics();
 	if (math1 == math2) return 0;
 	if (math1 > math2) return 1;
 	if (math1 < math2) return -1;
 }
 int compareHistory( Student* s1,  Student* s2) {
-	int history1 = s1->getMathsematics();
-	int history2 = s2->getMathsematics();
+	float history1 = s1->getMathsematics();
+	float history2 = s2->getMathsematics();
 	if (history1 == history2) return 0;
 	if (history1 > history2) return 1;
 	if (history1 < history2) return -1;
 }
 int compareAverage( Student* s1,  Student* s2) {
-	int average = s1->getAverage();
-	int average2 = s2->getAverage();
+	float average = s1->getAverage();
+	float average2 = s2->getAverage();
 	if (average == average2) return 0;
 	if (average > average2) return 1;
 	if (average < average2) return -1;
