c53af22b35de536b66ca839350c6027f34d322ff
MorariMaxim <119452648+MorariMaxim@users.noreply.github.com>
2023-02-28 19:30:41 +0200

    Update Student.cpp

--- lab2/2/Student.cpp
+++ lab2/2/Student.cpp
@@ -1,33 +1,29 @@
 #include "Student.h"
 
-int Student::getEnglish() {
+float Student::getEnglish() {
 	return english;
 }
-int Student::getHistory() {
+float Student::getHistory() {
 	return history;
 }
-
-int Student::getMathsematics() {
+float Student::getMathsematics() {
 	return mathematics;
 }
-void Student::setMathsematics(int grade) {
+void Student::setMathsematics(float grade) {
 	mathematics = grade;
 }
-void Student::setEnglish(int grade) {
+void Student::setEnglish(float grade) {
 	english = grade;
 }
-void Student::setHistory(int grade) {
+void Student::setHistory(float grade) {
 	history = grade;
 }
-
 char* Student::getName() {
 	return name;
 }
-
 void Student::setName(const char* toset) {
-	if (name != nullptr) delete name;
-	name = (char*)toset;
-	
+	if (name != nullptr) delete[] name;
+	name = (char*)toset;	
 }
 float Student::getAverage() {
 	return (float)(getMathsematics() + getEnglish() + getHistory()) / 3;
